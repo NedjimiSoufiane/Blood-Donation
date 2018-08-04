4bf75536ce071f60d86c2ebad37e69a9d98ca409
NedjimiSoufiane <d7devmobile@gmail.com>
2018-08-04 10:27:41 -0700

    first virsion 1.0.0

---
+++ app/src/main/java/com/sofianenedjimi19952411/sofiane/blood_donation/Main_lood_onation.java
@@ -0,0 +1,13 @@
+package com.sofianenedjimi19952411.sofiane.blood_donation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_lood_onation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_lood_onation);
+    }
+}
